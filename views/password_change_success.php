e425d40392ebefe9ada7a746f026d2fd1c055fd3
Lee Kar Sheng <karshenglee@icloud.com>
2016-05-11 09:23:00 +0000

    update

--- views/password_change_success.php
+++
@@ -1,1 +0,0 @@
-<h1>Password change successful!</h1>
--- views/quote_result.php
+++
@@ -1,2 +0,0 @@
-<h1>Quote Success!</h1>
-<p><?= "A share of ".htmlspecialchars($name)." (".htmlspecialchars($symbol).") costs <strong>$".htmlspecialchars($price)."</strong>."?></p>
